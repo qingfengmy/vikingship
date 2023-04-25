71e305e61ba4dbbb1a0a1ad91ac1adb4daacbe88
tao.zhang <qingfengmy@126.com>
2023-04-25 14:45:14 +0800

    no message

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -5,7 +5,7 @@ storiesOf('Welcome page', module)
   .add('welcome', () => {
     return (
       <>
-        <h1>欢迎来到 vikingship 组件库</h1>
+        <h1>欢迎来到 vikingshipppp 组件库</h1>
         <p>vikingship 是为慕课网课程打造的一套教学组件库，从零到一让大家去学习</p>
         <h3>安装试试</h3>
         <code>
